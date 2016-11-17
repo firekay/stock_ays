daede4a5b9cb6b635fc29a37048342e5f901ed63
kay <kayfire@163.com>
2016-11-17 18:58:36 +0800

    Add return now time.

--- utils/util.py
+++ utils/util.py
@@ -10,6 +10,8 @@ import datetime
 def get_today():
     return datetime.datetime.now().strftime('%Y%m%d')
 
+def get_time():
+    return datetime.datetime.now()
 
 def get_today_line():
     return datetime.datetime.now().strftime('%Y-%m-%d')
