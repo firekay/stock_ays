4188e0f2ab009efc592b72aa80769e3c152ac5d3
kay <kayfire@163.com>
2017-09-27 10:48:41 +0800

    Modify yesterday word

--- bin/main.sh
+++ bin/main.sh
@@ -3,3 +3,27 @@
 basepath=$(echo $(dirname $(pwd)))
 cd $basepath
 python main.py > console$(date +%Y%m%d) 2>&1
+# 全部时间全部股票历史交易数据(hist) save_all_days_all_stocks_hist_data
+python main.py -t-a-a-h-d > logs/console/save_all_days_all_stocks_hist_data$(date +%Y%m%d) 2>&1
+
+# 全部时间选择的股票历史交易数据(hist) save_all_days_select_stocks_hist_data
+python main.py -t-a-s-h-d > logs/console/save_all_days_select_stocks_hist_data$(date +%Y%m%d) 2>&1
+
+# 当天的全部股票的交易数据(hist) save_today_all_stocks_hist_data
+python main.py -t-a-s-h-d > logs/console/save_today_all_stocks_hist_data$(date +%Y%m%d) 2>&1
+
+# 当天的选择的股票的交易数据save_today_select_stocks_hist_data
+python main.py -t-t-s-h-d > logs/console/save_today_select_stocks_hist_data$(date +%Y%m%d) 2>&1
+
+# 昨天的全部股票的交易数据(hist) save_yestoday_all_stocks_hist_data
+python main.py -t-y-a-h-d > logs/console/save_yestoday_all_stocks_hist_data$(date +%Y%m%d) 2>&1
+
+# 昨天的选择的股票的交易数据save_yestoday_select_stocks_hist_data
+python main.py -t-y-s-h-d > logs/console/save_yestoday_select_stocks_hist_data$(date +%Y%m%d) 2>&1
+
+
+
+python main.py > $(date +%Y%m%d) 2>&1
+
+
+
--- service/base_service.py
+++ service/base_service.py
@@ -4,20 +4,13 @@
 import logging
 from utils.mysql_utils import *
 from models.model import *
+from service import table_service
+from service import data_service
 
 logger = logging.getLogger(__name__)
 
 
 # @conn
 def save_stocks_basic_data():
-    data_df = ts.get_stock_basics()
-    data_df['code'] = pd.Series(data_df.axes[0], index=data_df.index)
-    data = data_df.values
-    # logger.debug(data)
-    data_dicts = [{'code': row[22], 'name': row[0], 'industry': row[1], 'area': row[2], 'pe': row[3],
-                   'outstanding': row[4], 'totals': row[5], 'totalAssets':row[6], 'liquidAssets':row[7],
-                   'fixedAssets': row[8], 'reserved': row[9], 'reservedPerShare': row[10], 'eps': row[11],
-                   'bvps': row[12], 'pb': row[13], 'timeToMarket': row[14], 'undp': row[15],
-                   'perundp': row[16], 'rev': row[17], 'profit': row[18], 'gpr': row[19], 'npr': row[20],
-                   'holders': row[21], 'insert_date': today} for row in data]
-    StockBasic.insert_many(data_dicts).execute()
+    table_service.truncate_table(StockBasic)
+    data_service.save_stock_basic()
