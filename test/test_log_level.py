43f6f62496b656bfaa6c9c8ac1308a3486371c72
kay <kayfire@163.com>
2017-10-01 20:18:05 +0800

    Delete old tests

--- test/test_log_level.py
+++
@@ -1,9 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-
-def test_log_level():
-    logger.debug('debug')
-    logger.info('info')
-    logger.warn('warn')
-    logger.error('error')
