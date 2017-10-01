f44777096fef8f24ac1463cba96a101c57771f05
kay <kayfire@163.com>
2017-10-02 00:18:12 +0800

    Modify cls to model

--- service/table_service.py
+++ service/table_service.py
@@ -7,38 +7,38 @@ from models.model import *
 logger = logging.getLogger(__name__)
 
 
-def truncate_table(cls):
+def truncate_table(model):
     """清空表
     
     参数为model中的类名。
     """
-    logger.info('Truncate table %s' % cls)
-    cls.truncate_table()
+    logger.info('Truncate "table" %s' % model.__name__)
+    model.truncate_table()
 
 
-def create_table(cls):
+def create_table(model):
     """创建表。
 
     参数为model中的类名
     """
-    logger.info('Create table %s.' % cls)
-    cls.create_table()
+    logger.info('Create table %s.' % model.__name__)
+    model.create_table()
 
 
 def create_tables(classes):
     """参数为列表， 列表中数据是model中的类名"""
-    [create_table(cls) for cls in classes]
+    [create_table(model) for model in classes]
 
 
-def drop_table(cls):
+def drop_table(model):
     """删除单个表, 参数为model中的类名"""
-    logger.info('Drop talbe %s.' % cls)
-    cls.drop_table(fail_silently=True)
+    logger.info('Drop talbe %s.' % model.__name__)
+    model.drop_table(fail_silently=True)
 
 
 def drop_tables(classes):
     """参数为列表， 列表中数据是model中的类名"""
-    [drop_table(cls) for cls in classes]
+    [drop_table(model) for model in classes]
 
 
 def create_all_tables():
