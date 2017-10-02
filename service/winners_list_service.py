2e01e96dc039a3a9eade03344185aedbcaf7c38a
kay <kayfire@163.com>
2017-10-02 12:25:14 +0800

    Add winners list service

---
+++ service/winners_list_service.py
@@ -0,0 +1,72 @@
+# encoding: UTF-8
+""""""
+
+import logging
+from models.model import *
+from dal import winners_list_dal as wllist_dal
+from dal import util_dal
+
+
+DAYS_TYPE = [5, 10, 30, 60]
+
+
+def save_top_list(date):
+    data_dicts = wllist_dal.get_top_list(date)
+    if data_dicts:
+        if util_dal.delete_date(TopList, date):
+            util_dal.save_data(TopList, data_dicts)
+
+
+def save_individual_statistics_tops(days_type=None):
+    def _save_individual_statistics_tops(day_type):
+        data_dicts = wllist_dal.get_individual_statistics_tops(day_type)
+        if data_dicts:
+            if util_dal.delete_insert_date_days_type_data(IndividualStatisticsTops, today, day_type):
+                util_dal.save_data(IndividualStatisticsTops, data_dicts)
+    if not days_type:
+        for d in DAYS_TYPE:
+            _save_individual_statistics_tops(d)
+    elif isinstance(days_type, list):
+        for d in days_type:
+            _save_individual_statistics_tops(d)
+    else:
+        _save_individual_statistics_tops(days_type)
+
+
+def save_broker_tops(days_type=None):
+    def _save_broker_tops(day_type):
+        data_dicts = wllist_dal.get_broker_tops(day_type)
+        if data_dicts:
+            if util_dal.delete_insert_date_days_type_data(BrokerTops, today, day_type):
+                util_dal.save_data(BrokerTops, data_dicts)
+    if not days_type:
+        for d in DAYS_TYPE:
+            _save_broker_tops(d)
+    elif isinstance(days_type, list):
+        for d in days_type:
+            _save_broker_tops(d)
+    else:
+        _save_broker_tops(days_type)
+
+
+def save_institution_tops(days_type=None):
+    def _save_institution_tops(day_type):
+        data_dicts = wllist_dal.get_institution_tops(day_type)
+        if data_dicts:
+            if util_dal.delete_insert_date_days_type_data(InstitutionTops, today, day_type):
+                util_dal.save_data(InstitutionTops, data_dicts)
+    if not days_type:
+        for d in DAYS_TYPE:
+            _save_institution_tops(d)
+    elif isinstance(days_type, list):
+        for d in days_type:
+            _save_institution_tops(d)
+    else:
+        _save_institution_tops(days_type)
+
+
+def save_institution_detail():
+    data_dicts = wllist_dal.get_institution_detail()
+    if data_dicts:
+        if util_dal.delete_insert_date_data(InstitutionDetail, today):
+            util_dal.save_data(InstitutionDetail, data_dicts)
