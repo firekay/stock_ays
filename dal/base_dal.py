73b41d1ee08799286171c428db6455a31d455857
kay <kayfire@163.com>
2017-10-02 16:05:10 +0800

    Modify stock basic fields

--- dal/base_dal.py
+++ dal/base_dal.py
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 
 from models.model import *
+import pandas as pd
 import tushare as ts
 import logging
 
@@ -25,10 +26,12 @@ def save_stock_basic():
         data_df = ts.get_stock_basics()
         data_df['code'] = pd.Series(data_df.axes[0], index=data_df.index)
         data = data_df.values
-        data_dicts = [{'code': row[15], 'name': row[0], 'industry': row[1], 'area': row[2], 'pe': row[3],
+        data_dicts = [{'code': row[22], 'name': row[0], 'industry': row[1], 'area': row[2], 'pe': row[3],
                        'outstanding': row[4], 'totals': row[5], 'totalAssets':row[6], 'liquidAssets':row[7],
                        'fixedAssets': row[8], 'reserved': row[9], 'reservedPerShare': row[10], 'eps': row[11],
-                       'bvps': row[12], 'pb': row[13], 'timeToMarket': row[14], 'insert_date': today_line}
+                       'bvps': row[12], 'pb': row[13], 'timeToMarket': row[14], 'undp': row[15],
+                       'perundp':row[16], 'rev': row[17], 'profit': row[18], 'gpr': row[19],
+                       'npr': row[20], 'holders': row[21], 'insert_date': today_line}
                       for row in data]
         StockBasic.insert_many(data_dicts).execute()
     except Exception:
