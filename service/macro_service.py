4e967d0c2a92e8f9ffb431810601541be68457d3
kay <kayfire@163.com>
2017-10-02 22:53:52 +0800

    Add function ducuments

--- service/macro_service.py
+++ service/macro_service.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def save_deposit_rate():
+    """得到存款利率"""
     table_service.truncate_table(DepositsRate)
     data_dicts = macro_dal.get_deposit_rate()
     if data_dicts:
@@ -18,6 +19,7 @@ def save_deposit_rate():
 
 
 def save_loan_rate():
+    """得到贷款利率"""
     table_service.truncate_table(LoanRate)
     data_dicts = macro_dal.get_loan_rate()
     if data_dicts:
@@ -25,6 +27,7 @@ def save_loan_rate():
 
 
 def save_required_reserves_rate():
+    """得到存款准备金率"""
     table_service.truncate_table(RequiredReservesRate)
     data_dicts = macro_dal.get_required_reserves_rate()
     if data_dicts:
@@ -32,6 +35,7 @@ def save_required_reserves_rate():
 
 
 def save_money_supply():
+    """得到货币供应量"""
     table_service.truncate_table(MoneySupply)
     data_dicts = macro_dal.get_money_supply()
     if data_dicts:
@@ -39,6 +43,7 @@ def save_money_supply():
 
 
 def save_money_supply_bal():
+    """得到货币供应量(年底余额)"""
     table_service.truncate_table(MoneySupplyBal)
     data_dicts = macro_dal.get_money_supply_bal()
     if data_dicts:
@@ -46,6 +51,7 @@ def save_money_supply_bal():
 
 
 def save_gdp_year():
+    """国内生产总值(年度)"""
     table_service.truncate_table(GrossDomesticProductYear)
     data_dicts = macro_dal.get_gdp_year()
     if data_dicts:
@@ -53,6 +59,7 @@ def save_gdp_year():
 
 
 def save_gdp_quarter():
+    """得到国内生产总值(季度)"""
     table_service.truncate_table(GrossDomesticProductQuarter)
     data_dicts = macro_dal.get_gdp_quarter()
     if data_dicts:
@@ -60,6 +67,7 @@ def save_gdp_quarter():
 
 
 def save_gdp_three_demands():
+    """三大需求对GDP贡献"""
     table_service.truncate_table(GdpThreeDemands)
     data_dicts = macro_dal.get_gdp_three_demands()
     if data_dicts:
@@ -67,6 +75,7 @@ def save_gdp_three_demands():
 
 
 def save_gdp_three_industry_pull():
+    """三大产业对GDP拉动"""
     table_service.truncate_table(GdpThreeIndustryPull)
     data_dicts = macro_dal.get_gdp_three_industry_pull()
     if data_dicts:
@@ -74,6 +83,7 @@ def save_gdp_three_industry_pull():
 
 
 def save_gdp_three_industry_contrib():
+    """三大产业贡献率"""
     table_service.truncate_table(GdpThreeIndustryContrib)
     data_dicts = macro_dal.get_gdp_three_industry_contrib()
     if data_dicts:
@@ -81,6 +91,7 @@ def save_gdp_three_industry_contrib():
 
 
 def save_cpi():
+    """居民消费价格指数"""
     table_service.truncate_table(CPI)
     data_dicts = macro_dal.get_cpi()
     if data_dicts:
@@ -88,6 +99,7 @@ def save_cpi():
 
 
 def save_ppi():
+    """工业品出厂价格指数"""
     table_service.truncate_table(PPI)
     data_dicts = macro_dal.get_ppi()
     if data_dicts:
