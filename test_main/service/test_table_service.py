60d5b4f3d736f820619e53446c7da6d3260eff27
kay <kayfire@163.com>
2017-10-02 19:18:59 +0800

    Modify test table service

--- test_main/service/test_table_service.py
+++ test_main/service/test_table_service.py
@@ -4,22 +4,44 @@ from service import table_service
 from models.model import *
 
 
-table_model = PerformanceReport
-table_models = []
+table_model = DistributionPlans
+
+transaction_models = [HistoryKDataD, HistoryKDataW, HistoryKDataM, HistoryDataD,
+                      HistoryDataW, HistoryDataM, RevoteHistoryData, TodayAllData,
+                      TickData, BigIndexData, BigTradeData]
+investment_ref_models = [DistributionPlans, PerformanceForecast, RestrictedStock,
+                         FundHoldings, NewStocks, FinancingSecuritiesSh,
+                         FinancingSecuritiesDetailSh, FinancingSecuritiesSz,
+                         FinancingSecuritiesDetailSz]
+classification_models = [IndustryClassified, ConceptClassified, SmeClassified,
+                         AreaClassified, GemClassified, StClassified, Hs300, Sz50,
+                         Zz500, Terminated, Suspend]
+base_models = [StockBasic, PerformanceReport, ProfitAbility, OperationAbility, GrowthAbility,
+               PayDebtAbility, CashFlow]
+macro_models = [DepositsRate, LoanRate, RequiredReservesRate, MoneySupply, MoneySupplyBal,
+                GrossDomesticProductYear, GrossDomesticProductQuarter, GdpThreeDemands,
+                GdpThreeIndustryPull, GdpThreeIndustryContrib, CPI, PPI]
+winners_list_models = [TopList, IndividualStatisticsTops, BrokerTops, InstitutionTops,
+                       InstitutionDetail]
+bank_loan_models = [ShiborRate, ShiborQuote, ShiborMA, LPR, LprMA]
 
 
 def test_create_table():
     table_service.create_table(table_model)
 
 
-def test_create_tables():
-    table_service.create_tables(table_models)
-
-
 def test_drop_table():
     table_service.drop_table(table_model)
 
 
+def test_create_tables():
+    table_service.create_tables(investment_ref_models)
+
+
 def test_drop_tables():
-    table_service.drop_tables(table_models)
+    table_service.drop_tables(investment_ref_models)
+
+
+def test_drop_all_tables():
+    table_service.drop_all_tables()
 
