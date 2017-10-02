cb9ca399ccef74b903ed594a9c0e01c84b720180
kay <kayfire@163.com>
2017-10-02 12:23:26 +0800

    Rename dal_util to util_dal and func delete data year

--- dal/util_dal.py
+++ dal/util_dal.py
@@ -29,7 +29,52 @@ def delete_data(model, year, quarter):
         return True
 
 
-def delete_insert_data(model, insert_date):
+def delete_year(model, year):
+    logger.info('Begin delete %s data, year is: %s.' % (model.__name__, year))
+    try:
+        TopList.delete().where(model.date == date).execute()
+    except Exception:
+        logger.exception('Error delete %s data, year is: %s.' % (model.__name__, year))
+        return False
+    else:
+        logger.info('Success delete %s data, year is: %s.' % (model.__name__, year))
+        return True
+
+
+def delete_date(model, date):
+    logger.info('Begin delete %s data, insert date is: %s.' % (model.__name__, date))
+    try:
+        TopList.delete().where(model.date == date).execute()
+    except Exception:
+        logger.exception('Error delete %s data, insert date is: %s.' % (model.__name__, date))
+        return False
+    else:
+        logger.info('Success delete %s data, insert date is: %s.' % (model.__name__, date))
+        return True
+
+
+def delete_insert_date_days_type_data(model, insert_date, days_type):
+    """删除插入时间的数据
+    
+    Args:
+        model: peewee定义的model, models.model.py中定义的
+        insert_date: 插入时间
+        days_type: 天的类型类型, e.g.: 5, 10, 30, 60
+    """
+    logger.info('Begin delete %s data, insert date is: %s.' % (model.__name__, insert_date))
+    try:
+        model.delete().where(model.insert_date == insert_date,
+                             model.days_type == days_type)\
+            .execute()
+    except Exception:
+        logger.exception('Error delete %s data, insert date is: %s.' % (model.__name__, insert_date))
+        return False
+    else:
+        logger.info('Success delete %s data, insert date is: %s.' % (model.__name__, insert_date))
+        return True
+
+
+def delete_insert_date_data(model, insert_date):
     """删除插入时间的数据
     
     Args:
