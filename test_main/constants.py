481ca844fa88bd4e40aca06ff205264ed78f9a1f
kay <kayfire@163.com>
2017-10-03 11:41:58 +0800

    Add some constants to test_main

--- test_main/constants.py
+++ test_main/constants.py
@@ -1,2 +1,8 @@
-YEAR = 2017
+from utils import util
 QUARTER = 2
+YEAR = util.get_year()
+MONTH = util.get_month()
+TODAY = util.get_today()
+TODAY_LINE = util.get_today_line()
+QUARTER = 2
+DATE = '2017-09-29'
